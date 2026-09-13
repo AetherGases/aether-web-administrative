937e8c88374fe1c89afd29b480f49d6277d9e14d
Lais <laisalmeida0143@institutojef.org.com>
2026-09-13 12:21:34 -0300

    projeto tirei .env

--- Servelet1ano/src/main/java/com/example/servelet1ano/connection/ConnectionFactory.java
+++ Servelet1ano/src/main/java/com/example/servelet1ano/connection/ConnectionFactory.java
@@ -1,45 +1,31 @@
 package com.example.servelet1ano.connection;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-//Isso foi a IA que falou para colocar (pq eu pedi pra ela fazer com .env)
-
 public class ConnectionFactory {
-    private static final Dotenv dotenv = Dotenv.load();
+    public static Connection connect(){
 
-    private static final String URL = String.format(
-            "jdbc:postgresql://%s:%s/%s",
-            dotenv.get("DB_HOST"),
-            dotenv.get("DB_PORT"),
-            dotenv.get("DB_NAME")
-    );
-
-    private static final String USUARIO = dotenv.get("DB_USER");
-    private static final String SENHA = dotenv.get("DB_PASSWORD");
-    private static final String DRIVER = dotenv.get("DB_DRIVER");
-
-    public static Connection connect() throws SQLException {
         try {
-            Class.forName(DRIVER);
-            return DriverManager.getConnection(URL, USUARIO, SENHA);
-        } catch (ClassNotFoundException e) {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://pg-af463ee-laisalmeida0143-375e.j.aivencloud.com:24903/db_aether?sslmode=require", "avnadmin", " ");
+
+            return conn;
+        }
+        catch (ClassNotFoundException | SQLException e){
             e.printStackTrace();
-            throw new SQLException("Driver PostgreSQL não encontrado");
+            return null;
         }
     }
 
-    public static void main(String[] args) {
+
+    public static void disconnect(Connection conn){
         try {
-            Connection conn = connect();
-            if (conn != null && !conn.isClosed()) {
-                System.out.println("Conexão com sucesso");
+            if (conn != null || !conn.isClosed()){
                 conn.close();
             }
-        } catch (SQLException e) {
-            System.out.println("Erro de conexão: " + e.getMessage());
+        }catch (SQLException e){
             e.printStackTrace();
         }
     }
